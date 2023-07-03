a549fa3d7de2d8fa0c87ff518eca7b93172b8d29
navepe <nave@webz.io>
2023-07-03 11:21:51 +0300

    monitor

--- monitor_redis_connections_bash.sh
+++ monitor_redis_connections_bash.sh
@@ -8,7 +8,6 @@ current_connections=$(redis-cli info clients |grep -Eo '[0-9]{0,5}' |awk NR==1)
 limit=$(echo "$(($max_connections * 80/100))")
 ports=(7000 7001 7002)
 hostname=$(echo "$HOSTNAME")
-redis_instance="redis-001"
 
 echo limit=$limit
 echo current_connections=$current_connections
@@ -36,14 +35,38 @@ if ! [[ -n $current_connections || $limit ]]; then
   fi
 fi
 
-if [[ $hostname == $redis_instance ]]; then
-	for port in "${ports[@]}"; do
-		current_port=$(redis-cli -p "${port}" info clients |grep -Eo '[0-9]{0,5}' |awk NR==1)
-		if [[ $current_port ]]; then
-			echo "Redis is running on ${port}"
-		else
-			echo "Redis is not running on this ${port}"
+if [[ $hostname == "redis-001" ]]; then   ### if hostname is redis-001
+	for port in "${ports[@]}"; do    ### run on ports 7000, 7001 & 7002
+		current_connection_port=$(redis-cli -p "${port}" info clients |grep -Eo '[0-9]{0,5}' |awk NR==1) ### check how many connection + specific port
+		max_connections_port=$(redis-cli -p "${port}" config get maxclients |grep -Eo '[0-9]{0,5}')    ### check what is the max connections + port
+		limit_port=$(echo "$(($max_connections_port * 80/100))")    ### calculate limit
+		
+		echo limit=$limit_port
+		echo current_connections=$current_connection_port
+
+		if [[ $current_connection_port -gt $limit_port ]]; then  ### if the current is bigger than 80%
+			message_port="$HOSTNAME"'-Too Many Connections in redis' ### create a msg
+			if [[ " ${nbd_env[*]} " =~ "redis-001" ]]; then   ### if the instance is member of nbd env - send a msg in the channel
+				python /home/omgili/scripts/send_message_to_slack.py "$message_port" "nbd_alerts_channel"
+			elif [[ " ${sparta_env[*]} " =~ "redis-001" ]]; then   ### sparta env
+				python /home/omgili/scripts/send_message_to_slack.py "$message_port" "sparta_alerts_channel"
+			else
+				python /home/omgili/scripts/send_message_to_slack.py "$message_port" "devops_alerts_channel" ### the rest send to devops
+			fi
 		fi
+
+		if ! [[ -n $current_connectionn_port || $limit_port ]]; then  ### if there is no value in current or limit - too many connection and it failes
+			connection_failed_port="$HOSTNAME"'- Failed to connect to redis'
+		        if [[ " ${nbd_env[*]} " =~ "redis-001" ]]; then
+			        python /home/omgili/scripts/send_message_to_slack.py "$connection_failed_port" "nbd_alerts_channel"
+		        elif [[ " ${sparta_env[*]} " =~ "redis-001" ]]; then
+			        python /home/omgili/scripts/send_message_to_slack.py "$connection_failed_port" "sparta_alerts_channel"
+		        else
+			        python /home/omgili/scripts/send_message_to_slack.py "$connection_failed_port" "devops_alerts_channel"
+			fi
+		fi
+		    
+
 	done
 fi
 
